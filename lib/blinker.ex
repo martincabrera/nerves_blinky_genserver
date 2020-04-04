6c4f9a20ce456e699d85a570cad44898d6fd356c
Martin Cabrera <martin@kisland.com>
2020-04-04 14:26:19 +0200

    adds GenServer for Blinker

---
+++ lib/blinker.ex
@@ -0,0 +1,45 @@
+defmodule NetworkLed.Blinker do
+  user GenServer
+
+  @moduledoc """
+  Simple GenServer to control GPIO #18
+  """
+
+  require Logger
+  alias Nerves.Leds
+
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state) do
+    enable()
+
+    {:ok, state}
+  end
+
+  def handle_cast(:enable, state) do
+    Logger.info("Enabling LED")
+    Leds.set(green: true)
+
+    {:noreply, state}
+  end
+
+  def handle_cast(:disable, state) do
+    Logger.info("Disabling LED")
+    Leds.set(green: faslse)
+
+    {:noreply, state}
+  end
+
+
+  def enable() do
+    GenServer.cast(__MODULE__, :enable)
+  end
+
+  def disable() do
+    GenServer.cast(__MODULE__, :disable)
+  end
+
+end
