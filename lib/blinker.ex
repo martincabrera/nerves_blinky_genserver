f09b1e355b90567680774450cb5176dc07162d7c
Martin Cabrera <martin@kisland.com>
2020-04-06 20:35:51 +0200

    fixes GenServer

--- lib/blinker.ex
+++ lib/blinker.ex
@@ -8,38 +8,41 @@ defmodule NetworkLed.Blinker do
   require Logger
   alias Nerves.Leds
 
-
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def init(state) do
-    enable()
-
+  def init(_state) do
+    led_on()
+    state = :off
+    Logger.info("In INIT: Disabling LED")
+    Process.send_after(self(), state, 2_000)
     {:ok, state}
   end
 
-  def handle_cast(:enable, state) do
+  def handle_info(:off, _state) do
+    Logger.info("In handle_call off: Disabling LED")
+    led_off()
+    new_state = :on
+    Process.send_after(self(), new_state, 2_000)
+    {:noreply, new_state}
+  end
+
+  def handle_info(:on, _state) do
+    Logger.info("In handle_call on: Enabling LED")
+    led_on()
+    new_state = :off
+    Process.send_after(self(), new_state, 2_000)
+    {:noreply, new_state}
+  end
+
+  defp led_on do
     Logger.info("Enabling LED")
     Leds.set(green: true)
-
-    {:noreply, state}
   end
 
-  def handle_cast(:disable, state) do
+  defp led_off do
     Logger.info("Disabling LED")
     Leds.set(green: false)
-
-    {:noreply, state}
   end
-
-
-  def enable() do
-    GenServer.cast(__MODULE__, :enable)
-  end
-
-  def disable() do
-    GenServer.cast(__MODULE__, :disable)
-  end
-
 end
